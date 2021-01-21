c63143cdbab5931fd34012add37e55e01dcf2305
Anton Georgesco <antongeorgescu@gmail.com>
2021-01-21 17:53:49 -0500

    updates to display image

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,11 @@ router.get('/', function(req, res, next) {
   res.send({team : `My team is composed from ${myteam.join(',')}`});
 });
 
+/* GET an image */
+router.get('/image/:pageNo', function(req, res, next) {
+  var imgfile = '84605-Cape-Breton-Highlands-National-Park.jpg';
+  var imgpath = `${req.protocol}://${req.get('host')}/images/${imgfile}`;
+  res.send({image : `${imgpath}`});
+});
 
 module.exports = router;
